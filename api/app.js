23836e5ed5039314c0406faac60bb16aa911d785
Sajjadmoghadam <140229578+Sajjadmoghadam@users.noreply.github.com>
2025-02-25 17:39:26 +0330

    create file structures in my project

--- api/app.js
+++ api/app.js
@@ -1,8 +1,13 @@
 import express from "express"
 import morgan from "morgan"
 import cors from "cors"
+import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv"
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
 const app = express()
+app.use("/media", express.static(path.join(__dirname, "Public/media")));
 dotenv.config({path:"./.env"})
 app.use(express.json())
 app.use(morgan("dev"))
